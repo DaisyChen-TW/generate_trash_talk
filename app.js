5fef826d8cec7d706d476db4e7b902a44897e4f6
Daisy <qoo8857300@gmail.com>
2021-02-03 01:38:34 +0800

    first-upload

---
+++ app.js
@@ -0,0 +1,31 @@
+//require packages used in the project
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const generateTrashTalk = require('./generate_trash_talk')
+const app = express()
+const port = 3000
+
+//setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// setting routes
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const options = req.body.job
+  const TrashTalk = generateTrashTalk(options)
+  res.render('index',{ TrashTalk })
+})
+//  console.log('generateTrashTalk', {generateTrashTalk: TrashTalk})
+
+// starts the express server and listening for connections.
+app.listen(port, () => {
+  console.log(`Express app listening on http://localhost:${port}.`)
+})
---
+++ generate_trash_talk.js
@@ -0,0 +1,71 @@
+  // define sample function to randomly return a item in an array
+  function sample(array) {
+  const index = Math.floor(Math.random() * array.length)
+  return array[index]
+}
+
+function generateTrashTalk(options) {
+  
+  const task = {
+      engineer: ['加個按鈕','加新功能','切個版', '改一點 code'],
+      designer: ['畫一張圖', '改個 logo','順便幫忙設計一下','隨便換個設計'],
+      entrepreneur: ['週末加班', '要能賺錢','想個 business model','找 VC 募錢']
+  }
+
+  const phrase = ['很簡單','很容易','很快','很正常']
+
+  //可隨機產生工程師的task
+  const engineerTask = sample(task.engineer)
+  
+  //可隨機產生設計師的task
+  const designerTask = sample(task.designer)
+
+  //可隨機產生企業家的task
+  const entrepreneurTask = sample(task.entrepreneur)
+  
+  //以下可以使用${thePhrace}，輸出'很簡單','很容易','很快','很正常'
+  const thePhrace = sample(phrase)
+  
+
+  //試試看else if的方式
+  if (options === 'engineer') {
+    return (`身為一位工程師，${engineerTask} + ${thePhrace}吧！`)
+
+  } else if (options === 'designer'){
+    return (`身為一位設計師，${designerTask} + ${thePhrace}吧！`)
+
+  } else if (options === 'entrepreneur'){
+    return (`身為一位企業家，${entrepreneurTask} + ${thePhrace}吧！`)
+    
+  } else {
+    return ('請點選任一工作')
+  }
+
+
+  //針對工程師的幹話
+
+  //if (options.job === '工程師') {
+
+    //return console.log(`身為一位工程師，${engineerTask} + ${thePhrace}吧！`)
+  //}
+  
+  //針對設計師的幹話
+  //if (options.job === '設計師') {
+    //return console.log(`身為一位設計師，${designerTask} + ${thePhrace}吧！`)
+  //}
+  
+
+  //針對企業家的幹話
+  //if (options.job === '企業家') {
+    //return console.log(`身為一位企業家，${entrepreneurTask} + ${thePhrace}吧！`)
+  //}    
+  
+  // let randomTrashTalk = `身為一個${options.job},${trashCollection},${sample(phrase)}吧!`
+  // return randomTrashTalk
+}
+
+//generateTrashTalk()
+
+// export generatePassword function for other files to use
+module.exports = generateTrashTalk
+
